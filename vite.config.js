d7e9f8351a1ad587a14a5c8c72ae5a30ae6cb893
vijaykolekar137 <vijaykolekar137@gmail.com>
2023-12-11 23:39:09 +0530

    test6

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  //base: "/glinks/",
-  plugins: [react()],
-  base: '/glinks/'
+  base: '/glinks/',
+  plugins: [react()]
 })
 
